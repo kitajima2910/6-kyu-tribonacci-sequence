2e0a751ced0fcb48c3020ce22850a632977f2ce1
kitajima2910 <50172777+kitajima2910@users.noreply.github.com>
2023-10-02 15:05:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 6-kyu-tribonacci-sequence
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/6-kyu-tribonacci-sequence)
---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+
+[1, 1, 1] => [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+[0, 0, 1] => [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+
+*/
+
+function tribonacci(signature, n) {
+  if (n >= 0 && n < signature.length) {
+    return signature.splice(0, n);
+  }
+
+  for (let i = 0; signature.length < n; i++) {
+    signature.push(signature[i] + signature[i + 1] + signature[i + 2]);
+  }
+
+  return signature;
+}
+
+console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([0, 0, 1], 10));
+console.log(tribonacci([0, 1, 1], 10));
+console.log(tribonacci([1, 0, 0], 10));
+console.log(tribonacci([0, 0, 0], 10));
+console.log(tribonacci([1, 2, 3], 10));
+console.log(tribonacci([3, 2, 1], 10));
+console.log(tribonacci([1, 1, 1], 1));
+console.log(tribonacci([300, 200, 100], 0));
